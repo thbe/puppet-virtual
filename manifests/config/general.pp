f1fec14c6993ac944853a0d60f0d54eea8334eb1
thbe <project@bendler-net.de>
2015-10-28 13:51:11 +0100

    Add general classes to fix build error

---
+++ manifests/config/general.pp
@@ -0,0 +1,8 @@
+# Class: virtual::config::general
+#
+# This class contain the general config configuration for virtual machines
+#
+# Parameters: This class has no parameters
+#
+class virtual::config::general {
+}
---
+++ manifests/package/general.pp
@@ -0,0 +1,8 @@
+# Class: virtual::package::general
+#
+# This class contain the general package configuration for virtual machines
+#
+# Parameters: This class has no parameters
+#
+class virtual::package::general {
+}
